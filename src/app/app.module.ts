4249edab0d00bcd105eb4164fee8d60bfaefa53c
Frederic Mandrea <fred@notanumber.fr>
2022-12-01 12:18:05 +0100

    add injection token example

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, InjectionToken } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,6 +6,9 @@ import { ProfileCardComponent } from './profile-card/profile-card.component';
 import { FullNamePipe } from './full-name.pipe';
 import { UserService } from './user.service';
 import { FormsModule } from '@angular/forms';
+import { countryList } from './models/countries';
+
+export const countryListToken = new InjectionToken("countryList");
 
 @NgModule({
   declarations: [
@@ -17,7 +20,13 @@ import { FormsModule } from '@angular/forms';
     BrowserModule,
     FormsModule
   ],
-  providers: [ UserService ],
+  providers: [ 
+    UserService,
+    { 
+      provide: countryListToken,
+      useValue: countryList
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/user.service.ts
+++ src/app/user.service.ts
@@ -1,4 +1,5 @@
-import { Injectable } from '@angular/core';
+import { Inject, Injectable } from '@angular/core';
+import { countryListToken } from './app.module';
 import { Person } from './models/person';
 import { profiles } from './resources/profiles';
 
@@ -6,7 +7,8 @@ import { profiles } from './resources/profiles';
 export class UserService {
   private _users: Person[] = [];
 
-  constructor() {
+  constructor(@Inject(countryListToken) public maListedePays: any[]) {
+    console.log(this.maListedePays)
     this._users = profiles;
   }
 
